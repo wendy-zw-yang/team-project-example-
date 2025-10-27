f67638cbec29ae8b00945e491b6313df6129fd0a
ace-yang <ace_yang@sina.com>
2025-10-27 17:54:46 +0800

    Merged fix for #1
    Co-authored-by: wendy-zw-yang <wendy.zw.yang@outlook.com>

---
+++ src/main/java/com/mall/model/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * 模拟李四创建dev分支，并添加 HelloWorld.java 文件
+ */
+package com.mall.model;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
